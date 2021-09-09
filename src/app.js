a60478ddda9bc1a5f1a595748bb8f103b1c20e30
dussan <dussan.freire@ucb.edu.bo>
2021-09-09 09:56:00 -0400

    merge manual

--- src/app.js
+++ src/app.js
@@ -1,49 +1,93 @@
-const _service = require('./service/proyectoServicio')
-const express = require('express')
-const app = express()
-const port = 3000
-const service = new _service()
+const _service = require("./service/proyectoServicio");
+const _service_form = require("./service/form");
+const express = require("express");
+const app = express();
+const port = 3000;
+const service = new _service();
+const service_form = new _service_form();
 
 app.use(express.json());
 
-app.post('/create_proyecto', async (req, res) => { //Crear
+app.post("/create_proyecto", async (req, res) => {
+  //Crear
   try {
-    const nuevoProyecto = await service.create_proyecto(req.body)
-    res.status(201).json(req.body)
+    const nuevoProyecto = await service.create_proyecto(req.body);
+    res.status(201).json(req.body);
   } catch (err) {
-    res.status(404)
+    res.status(404);
   }
 });
-app.put('/update_proyecto/:id',async (req, res)=>{ //Actualizar
-    try {
-      let {id} = req.params
-      const proyectoActualizado = await service.update_proyecto(id, req.body)
-      res.status(200).json(proyectoActualizado.rows);
-    } catch (error) {
-      res.status(404)
-    }
+app.put("/update_proyecto/:id", async (req, res) => {
+  //Actualizar
+  try {
+    let { id } = req.params;
+    const proyectoActualizado = await service.update_proyecto(id, req.body);
+    res.status(200).json(proyectoActualizado.rows);
+  } catch (error) {
+    res.status(404);
+  }
 });
-app.get('/get_proyectos', async (req, res) => { //Obtener
+app.get("/get_proyectos", async (req, res) => {
+  //Obtener
   try {
     const nuevoProyecto = await service.get_proyectos(req);
     res.status(200).json(nuevoProyecto.rows);
   } catch (err) {
-      res.status(404)
+    res.status(404);
   }
-})
+});
 
-app.get('/get_proyecto/:id', async (req, res) => { //Obtener
+app.get("/get_proyecto/:id", async (req, res) => {
+  //Obtener
   try {
     const nuevoProyecto = await service.get_proyecto(req);
     res.status(200).json(nuevoProyecto.rows);
   } catch (err) {
-    res.status(404)
+    res.status(404);
   }
-})
+});
 
-
-
+//-----------------------------------------------yiga-------------------
+app.post("/extended_form", async (req, res) => {
+  try {
+    const newVolunteer = await service_form.register_changes(req.body);
+    let data_to_send = JSON.stringify(newVolunteer.rows[0]);
+    res.status(201).send(`{"message":"", "data": ${data_to_send}}`);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(400)
+      .send('{ "message": "Check the info that you sending", "data": ""}');
+  }
+});
+app.put("/extended_form/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    const changedVolunteer = await service_form.do_changes(id, req.body);
+    console.log(changedVolunteer);
+    res.status(202).send(`{"message":"Succesfully Updated!", "data":true}`);
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .send(`{"message":"Changes are not commited", "data":false}`);
+  }
+});
+app.get("/extended_form/:id", async (req, res) => {
+  try {
+    const newVolunteer = await service_form.get_volunteer_data(req);
+    let data_to_send = JSON.stringify(newVolunteer.rows[0]);
+    res.status(200).send(`{"message":"", "data": ${data_to_send}}`);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(204)
+      .send(
+        `{ "message": "The volunteer with id ${req.params[0]} does not exit"", "data": ""}`
+      );
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
