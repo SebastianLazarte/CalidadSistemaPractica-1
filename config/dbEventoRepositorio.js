c6c97ab7a01ea351e0e6b726e9680fc391cc56df
RickySandi <rickysandis@gmail.com>
2021-09-21 17:29:22 -0400

    Conexion a DB Heroku

--- config/dbEventoRepositorio.js
+++ config/dbEventoRepositorio.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "admin",
-  database: "postgres",
-  host: "localhost",
+  user: "hsazteibnsnquc",
+  password: "96c44f19b6a31a67521c2fa65c9233544ed1d7d5388367c6d9ff4c22c940a340", //use your pass my friend
+  database: "d5mjf648gc2p7f",
+  host: "ec2-54-156-24-159.compute-1.amazonaws.com",
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
