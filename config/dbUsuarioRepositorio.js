d0dc880cae1867537678c8268736c6f9b7d10d1f
dussan <dussan.freire@ucb.edu.bo>
2021-10-15 17:48:33 -0400

    se agrego cualidades

--- config/dbUsuarioRepositorio.js
+++ config/dbUsuarioRepositorio.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "kucbjwwgviyzmk",
-  password: "a52262572f4bc3c60cc7947fed57d4b618fd1aa8a0a05d6615666ae106e087e9", //use your pass my friend
-  database: "d2t7r859pdvtd5",
-  host: "ec2-107-20-24-247.compute-1.amazonaws.com",
+  user: "hgpmlfhmjxvnfr",
+  password: "e3fcf341e4ff4a68075b951e1c9a75239afaa42d7eccc3e9c7db81bda6c77a05", //use your pass my friend
+  database: "d966qfatdj765h",
+  host: "ec2-54-173-138-144.compute-1.amazonaws.com",
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
