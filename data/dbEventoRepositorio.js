823effd12325ad937e398ecb713c5f10ae2206c7
dussan <dussan.freire@ucb.edu.bo>
2021-12-05 12:07:12 -0400

    se dividio la carpeta en 2 y se refactorizo el pool para que este sea solo uno y no 3

--- data/dbEventoRepositorio.js
+++ data/dbEventoRepositorio.js
@@ -1,15 +1,4 @@
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-  user: "hgpmlfhmjxvnfr",
-  password: "e3fcf341e4ff4a68075b951e1c9a75239afaa42d7eccc3e9c7db81bda6c77a05", //use your pass my friend
-  database: "d966qfatdj765h",
-  host: "ec2-54-173-138-144.compute-1.amazonaws.com",
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const { pool } = require("../config/pool.config");
 
 module.exports = pool;
 class DbEventoRepositorio {
