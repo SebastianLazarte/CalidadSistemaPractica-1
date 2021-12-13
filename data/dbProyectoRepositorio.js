5cc3ee8bc53f9129d2ec282ac07fbbe50557c9db
mabatule1999 <maba1998a@gmail.com>
2021-12-13 13:13:01 -0400

    prueba

--- data/dbProyectoRepositorio.js
+++ data/dbProyectoRepositorio.js
@@ -26,7 +26,10 @@ class DbProyectoRepositorio {
     );
     return proyecto;
   }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 1f5b432 (prueba)
   async create_proyecto(data) {
     debugger;
     const {
@@ -56,53 +59,42 @@ class DbProyectoRepositorio {
     let fecI;
     let fecF;
     let newEstado;
-    let fechaActual = new Date();
-    if (fecha_inicio == "") {
-      fecI = fechaActual;
-    } else {
-      const [yearI, monthI, dayI] = fecha_inicio.split("-");
-      fecI = new Date(monthI + " " + dayI + " " + yearI);
+    let fechaActual= new Date();
+
+    if(fecha_inicio!=""){
+      const [yearI, monthI,dayI ] = fecha_inicio.split("-")
+      fecI= new Date(monthI+' '+dayI+' '+yearI);  
+    }else{
+      fecI = fechaActual;  
     }
 
-    if (fecha_fin == "") {
-      if (estado) {
-        fecF = null;
-        newEstado = true;
-      } else {
-        fecF = fechaActual;
-        newEstado = false;
-      }
-    } else {
-      const [yearF, monthF, dayF] = fecha_fin.split("-");
-      fecF = new Date(monthF + " " + dayF + " " + yearF);
+    if(estado){
 
-      if (fecF < fechaActual) {
-        newEstado = false;
-      } else {
-        newEstado = true;
+      if(fecha_fin!=""){
+        const [yearF, monthF,dayF ] = fecha_fin.split("-")
+        fecF=new Date(monthF+' '+dayF+' '+yearF);
+        //ve el caso de la fecha final sea menor a la actual dependiendo de eso es un proyecto pasado o en curso
+        if(fecF < fechaActual ){
+          newEstado=false;
+        }else{
+          newEstado = true;
+        }
+        //el caso que la fecha inicial es mayor a la final
+        if(fecF<fecI){
+          fecF=null;
+        }
+      }else{
+        fecF=null;
+      }
+    }else{
+      if(fecI > fechaActual){
+        fecF=null;
+        newEstado=true;
+      }else{
+        fecF=fechaActual;
+        newEstado=false;
       }
     }
-    const new_proyeto = await pool.query(
-      "INSERT INTO proyectos(titulo, descripcion, objetivo, lider, numero_participantes, estado, fecha_inicio,fecha_fin,categoria_id,visualizar,informacion_adicional,url_imagen)VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)",
-      [
-        titulo, //1
-        descripcion, //2
-        objetivo, //3
-        lider, //4
-        numero_participantes_oficial, //5
-        newEstado, //6
-        fecI, //7
-        fecF, //8
-        categoria_id, //9
-        true, //10
-        informacion_adicional, //11
-        url_imagen, //12
-      ]
-    );
-    const proyecto_to_show = await pool.query(
-      "SELECT p.*, tipo as categoria FROM proyectos as p INNER JOIN categoria_proyectos ON p.categoria_id = categoria_proyectos.id  ORDER BY ID DESC LIMIT 1"
-    );
-    return proyecto_to_show;
   }
 
   async update_proyecto(id, data) {
