67f22f9799bbb1edab8f723407a658e5ebc2a3ad
CC <73244043+Carlos9855@users.noreply.github.com>
2022-03-15 17:30:06 -0400

    Update pool.config.js

--- config/pool.config.js
+++ config/pool.config.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "hgpmlfhmjxvnfr",
-  password: "e3fcf341e4ff4a68075b951e1c9a75239afaa42d7eccc3e9c7db81bda6c77a05", //use your pass my friend
-  database: "d966qfatdj765h",
-  host: "ec2-54-173-138-144.compute-1.amazonaws.com",
+  user: "dzuskbzshbdkna",
+  password: "64a8ea20a16e3dc13445c55ae9c791bec47af524d3949783d7747e943b2fedf7", //use your pass my friend
+  database: "d7qd1m74q5go7f",
+  host: "ec2-54-158-26-89.compute-1.amazonaws.com",
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
