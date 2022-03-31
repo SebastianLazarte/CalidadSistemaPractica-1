f43631192cd22029cb2ccb18f70daad49cafccb6
Jacovzap <jacovzap@gmail.com>
2022-03-30 23:39:01 -0400

    Security Hostpot 2 solution
    It was fixed the use of the variable process.env.

--- config/dbUsuarioRepositorio.js
+++ config/dbUsuarioRepositorio.js
@@ -279,7 +279,7 @@ class DbUsuarioRepositorio {
   }
   async disableUser(id_user) {
     let user_to_disable = await pool.query(
-      DISABLE_USER_SQL,
+      process.env.DISABLE_USER_SQL,
       [id_user]
     );
     return user_to_disable.rowCount > 0;
